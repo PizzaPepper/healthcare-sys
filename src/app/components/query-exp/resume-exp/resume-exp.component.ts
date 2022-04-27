e2a8cc9d4571194b5e85ef254da22d55069ffdf3
PizzaPepper <supergonzalez360@gmail.com>
2022-04-27 00:51:21 -0700

    refactoring code request access exp

--- src/app/components/query-exp/resume-exp/resume-exp.component.ts
+++ src/app/components/query-exp/resume-exp/resume-exp.component.ts
@@ -1,25 +1,40 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import Person from 'src/app/models/Person';
 import { Router } from '@angular/router';
 import { ExpedientService } from 'src/app/services/expedient.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-resume-exp',
   templateUrl: './resume-exp.component.html',
   styleUrls: ['./resume-exp.component.css'],
 })
-export class ResumeExpComponent implements OnInit {
+export class ResumeExpComponent implements OnInit, OnDestroy {
   @Input() onSelectPerson!: Person | null;
   @Output() returnList = new EventEmitter<void>();
 
   private waiting: any;
+  public stateAccess: string = 'loading';
 
   constructor(
     private _router: Router,
-    private _expedientService: ExpedientService
+    private _expedientService: ExpedientService,
+    private toastr: ToastrService
   ) {}
+  ngOnDestroy(): void {
+    clearInterval(this.waiting);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getStatus();
+  }
 
   onReturnList(): void {
     this.returnList.emit();
@@ -29,6 +44,7 @@ export class ResumeExpComponent implements OnInit {
     this._router.navigate(['expedient', this.onSelectPerson!.expedient]);
   }
 
+  // ! @deprecated
   waitRequest(): void {
     const popup: any = document.querySelector('.overlay');
     popup.style.visibility = 'visible';
@@ -58,6 +74,7 @@ export class ResumeExpComponent implements OnInit {
     }, 5000);
   }
 
+  // ! @deprecated
   onCloseWait(): void {
     const popup: any = document.querySelector('.overlay');
     popup.style.visibility = 'hidden';
@@ -68,5 +85,52 @@ export class ResumeExpComponent implements OnInit {
       .subscribe();
 
     clearInterval(this.waiting);
+  }
+
+  getStatus(): void {
+    this.waiting = setInterval(() => {
+      this._expedientService
+        .getStatus(this.onSelectPerson!.expedient)
+        .subscribe({
+          next: (status: any) => {
+            this.stateAccess = status;
+          },
+          error: (err: any) => {
+            this._expedientService
+              .putStatusDefault(this.onSelectPerson!.expedient)
+              .subscribe();
+            clearInterval(this.waiting);
+            this.toastr.error('Something went wrong');
+          },
+        });
+    }, 5000);
+  }
+
+  requestAccess(): void {
+    if (this.stateAccess === 'loading') {
+      this.toastr.error('Be patient dammit!');
+      return;
+    }
+    if (this.stateAccess === 'waiting') {
+      this.toastr.info('Is already requested');
+      return;
+    }
+    if (this.stateAccess === 'accepted') {
+      this.goToExpedient();
+      return;
+    }
+
+    this.stateAccess = 'loading';
+
+    this._expedientService.putStatus(this.onSelectPerson!.expedient).subscribe({
+      next: (status: any) => {
+        this.toastr.success(status);
+      },
+      error: (err: any) => {
+        this.toastr.error('Something went wrong');
+        this.stateAccess = 'waiting';
+        clearInterval(this.waiting);
+      }
+    })
   }
 }
